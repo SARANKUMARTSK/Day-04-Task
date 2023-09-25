2958489f5b36631b8bf738719c9c315305fc5f21
SARANKUMARTSK <sarankumartsk@gmail.com>
2023-09-25 22:42:35 +0530

    Day 04 Task Added

---
+++ RESUME.js
@@ -0,0 +1,34 @@
+
+const myData={ 
+    "basics": {
+    "fname": "SARAN",
+    "mname": "KUMAR",
+    "lname": " T",
+    "E Mail Id": "sarankumartsk@gmail.com",
+    "Adress": "24/1 , Masaniamman Kovil Street , Anaimalai(Tk), Coimbatore(Dist),Tamilnadu 642104",
+    "Location": "Anaimalai",
+    },
+    "Personal Details": {
+    "Date Of Birth": "18.08.1998",
+    "Marital Status": "Single",
+    "Father Name": "M.THANGAVEL",
+    "Mother Name": "T.VISALATCHi",
+    },
+    "Education Qualification" :
+    {
+    "Qualification": "B.sc.Mathematics",
+    "Collage": "Sree Saraswathi Thyagaraja College , Pollachi ,Tamilnadu",
+    "Mark % in College": "75%",
+    "School": "Palaniammal Higher Secondary School, Kaliyapuram , Pollachi , Tamilnadu",
+    "10 th Mark": "85%",
+    "12 th Mark": "80%",
+    "Additional Courses": "Diplomo in Micreosoft Office Packages",
+    },
+    "Work Experiance" :
+    {
+    "Company Name": "Suguna Foods Private Limited , Udumalpet , Tiruppur , Tamilnadu",
+    "Job Role": "Senior Supervisor in Breeder Operations",
+    "Year Of Experiance": "5 Years",
+}
+    };
+console.log(myData);
